9f32026bcc9f745715addf5834844317d4da6ff1
Giovani Pedroso <giovanisfpv@gmail.com>
2022-10-12 19:43:46 -0300

    initial commit

---
+++ Debug/Src/main.d
@@ -0,0 +1,3 @@
+Src/main.o: ../Src/main.c \
+ /home/santana/STM32CubeIDE/workspace_1.10.1/stm32f4xx_drivers/drivers/Inc/stm32f411xx.h
+/home/santana/STM32CubeIDE/workspace_1.10.1/stm32f4xx_drivers/drivers/Inc/stm32f411xx.h:
---
+++ drivers/Inc/stm32f411xx.h
@@ -0,0 +1,20 @@
+/*
+ * stm32f411xx.h
+ *
+ *  Created on: Oct 12, 2022
+ *      Author: santana
+ */
+
+#ifndef INC_STM32F411XX_H_
+#define INC_STM32F411XX_H_
+
+
+/*
+ * Base memory of the flash and the sram
+ */
+#define FLASH_BASE_ADDR 		0x08000000U
+#define SRAM1_BASE_ADDR 		0x20000000U
+#define SRAM 					SRAM1_BASE_ADDR
+
+
+#endif /* INC_STM32F411XX_H_ */
